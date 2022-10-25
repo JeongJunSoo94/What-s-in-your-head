375df194cc882a6f2f898928820ebdba614790d6
YoonChan <psalms119165sin@gmail.com>
2022-10-25 14:07:20 +0900

    자라나는 식물, 카메라(찬우형) 수정중

--- What’s in your head/Assets/Resources/Scripts/KSU/Using/PlayerController.cs
+++ What’s in your head/Assets/Resources/Scripts/KSU/Using/PlayerController.cs
@@ -142,7 +142,7 @@ namespace KSU
             if (!photonView.IsMine)
             {
                 if(transform.parent != null)
-                    playerRigidbody.velocity = Vector3.zero;
+                    //playerRigidbody.velocity = Vector3.zero;
                 return;
             }else if(characterState.isOutOfControl)
             {
