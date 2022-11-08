60d635a78ef51f1c17c6ca0d94b06575861d77e9
JCW <72681774+Valzang@users.noreply.github.com>
2022-11-08 20:11:44 +0900

    스테이지 1-2 기본 오브젝트 완료

--- What’s in your head/Assets/Resources/Scripts/JCW/Object/Stage1/FlagCheck.cs
+++ What’s in your head/Assets/Resources/Scripts/JCW/Object/Stage1/FlagCheck.cs
@@ -9,15 +9,38 @@ namespace JCW.Object.Stage1
     {
         [Header("열릴 오브젝트의 BothButton 스크립트")] [SerializeField] BothButton bothButton;
         [Header("빛을 낼 오브젝트")] [SerializeField] Transform lightButton;
+        [Header("초기화 할 시간")] [SerializeField] [Range(0,50)] float resetTime = 5f;
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Nella") || other.CompareTag("Steady"))
             {
+                StopAllCoroutines();
                 ++bothButton.bothCount;
                 lightButton.GetChild(0).gameObject.SetActive(false);
-                lightButton.GetChild(1).gameObject.SetActive(true);
+                lightButton.GetChild(1).gameObject.SetActive(true);                
+                GetComponent<Animator>().SetBool("isCheck", true);
                 Destroy(this);
             }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Nella") || other.CompareTag("Steady"))
+            {
+                StartCoroutine(nameof(Reset));
+            }
+        }
+
+        IEnumerator Reset()
+        {
+            float curTime = 0f;
+            while(curTime < resetTime)
+            {
+                curTime += Time.deltaTime;
+                yield return null;
+            }
+            GetComponent<Animator>().SetBool("isCheck", true);
+            --bothButton.bothCount;
         }
     }
 }
